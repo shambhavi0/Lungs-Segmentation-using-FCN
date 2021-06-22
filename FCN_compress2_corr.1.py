add888d54560037185dc0cddb9a927286bf50518
shambhavi0 <58625160+shambhavi0@users.noreply.github.com>
2021-06-22 17:07:12 +0530

    Add files via upload

---
+++ FCN_compress2_corr.1.py
@@ -0,0 +1,523 @@
+import numpy as np
+import pandas as pd
+
+import json
+import sys
+from PIL import Image, ImageOps
+
+#from skimage.io import imread
+#from matplotlib import pyplot as plt
+import random
+
+import os
+#os.environ['KERAS_BACKEND'] = 'theano'
+#os.environ['THEANO_FLAGS'] ='mode=FAST_RUN,device=cpu'
+#os.environ['THEANO_FLAGS'] = 'mode=FAST_RUN, device=gpu0, floatX=float32, optimizer=fast_compile'
+
+from keras import models
+from keras.optimizers import SGD
+from keras.layers import Input, ZeroPadding2D
+from keras.layers.core import Activation, Flatten, Reshape
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.utils import np_utils
+from keras.applications import imagenet_utils
+
+path = 'results/'
+img_w = 512
+img_h = 512
+n_labels = 2
+
+Lung = [255,255,255]
+
+Sky = [128,128,128]
+Building = [128,0,0]
+Pole = [192,192,128]
+Road_marking = [255,69,0]
+Road = [128,64,128]
+Pavement = [60,40,222]
+Tree = [128,128,0]
+SignSymbol = [192,128,128]
+Fence = [64,64,128]
+Car = [64,0,128]
+Pedestrian = [64,64,0]
+Bicyclist = [0,128,192]
+Unlabelled = [0,0,0]
+
+
+n_train = 89
+n_test = 6
+n_val = 5
+
+def label_map(labels):
+    label_map = np.zeros([img_h, img_w, n_labels])    
+    for r in range(img_h):
+        for c in range(img_w):
+            label_map[r, c, labels[r][c]] = 1
+    return label_map
+
+def label_map1(labels):
+    label_map = np.zeros([img_h, img_w, n_labels])    
+    for r in range(img_h):
+        for c in range(img_w):
+            #print(labels[r][c])
+            if(labels[r][c] == Lung[0]):
+                label_map[r, c, 0] = 1
+            elif(labels[r][c] == Unlabelled[0]):
+                label_map[r, c, 1] = 1
+    return label_map
+
+
+import os
+
+def prep_data1(mode):
+    assert mode in {'test', 'train', 'val'}, \
+        'mode should be either \'test\' or \'train\''
+    data = []
+    label = []
+    
+    folder_path = path + mode
+
+    images_path = os.listdir(folder_path)
+
+    main_files = []
+    truth_files = []
+    
+    for n, image in enumerate(images_path):
+        src = os.path.join(folder_path, image)
+        if(src.find('_mask.png') == -1 and src.find('_dilate.png') == -1 and src.find('_predict.png') == -1):
+            main_files.append(src)
+        else:
+            truth_files.append(src)
+    n = n_train if mode == 'train' else n_test
+    
+    if(mode == 'val'):
+        n = n_val
+        
+    index = 0
+    for filename in main_files:
+        print(filename)
+        
+        truth_file = filename.split('.png')
+        
+        tfile = truth_file[0] + '_mask.png'
+        
+        print(tfile)
+        if(filename == ""):
+            break
+        
+        img1 = Image.open(filename)
+
+
+        img2 = Image.open(tfile)
+
+        index += 1
+        # create a new image and paste the resized on it
+        
+
+        #img, gt = [imread(path + mode + '/' + filename + '.png')], imread(path + mode + '-colormap/' + filename + '.png')
+        
+        img, gt = [np.array(img1,dtype=np.uint8)], np.array(img2,dtype=np.uint8)
+        data.append(np.reshape(img,(512,512,3)))
+        label.append(label_map1(gt))
+        sys.stdout.write('\r')
+        sys.stdout.flush()
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    data, label = np.array(data), np.array(label).reshape((n, img_h * img_w, n_labels))
+
+    print( mode + ': OK')
+    print( '\tshapes: {}, {}'.format(data.shape, label.shape))
+    print( '\ttypes:  {}, {}'.format(data.dtype, label.dtype))
+    #print( '\tmemory: {}, {} MB'.format(data.nbytes / 1048576, label.nbytes / 1048576))
+
+    return data, label
+
+
+
+def prep_data(mode):
+    assert mode in {'test', 'train'}, \
+        'mode should be either \'test\' or \'train\''
+    data = []
+    label = []
+    df = pd.read_csv(path + mode + '.csv')
+    n = n_train if mode == 'train' else n_test
+    for i, item in df.iterrows():
+        if i >= n:
+            break
+        img, gt = [imread(path + item[0])], np.clip(imread(path + item[1]), 0, 1)
+        data.append(np.reshape(img,(256,256,1)))
+        label.append(label_map(gt))
+        sys.stdout.write('\r')
+        sys.stdout.write(mode + ": [%-20s] %d%%" % ('=' * int(20. * (i + 1) / n - 1) + '>',
+                                                    int(100. * (i + 1) / n)))
+        sys.stdout.flush()
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+    data, label = np.array(data), np.array(label).reshape((n, img_h * img_w, n_labels))
+
+    print( mode + ': OK')
+    print( '\tshapes: {}, {}'.format(data.shape, label.shape))
+    print( '\ttypes:  {}, {}'.format(data.dtype, label.dtype))
+    print( '\tmemory: {}, {} MB'.format(data.nbytes / 1048576, label.nbytes / 1048576))
+
+    return data, label
+
+"""
+def plot_results(output):
+    gt = []
+    df = pd.read_csv(path + 'test.csv')
+    for i, item in df.iterrows():
+        gt.append(np.clip(imread(path + item[1]), 0, 1))
+
+    plt.figure(figsize=(15, 2 * n_test))
+    for i, item in df.iterrows():
+        plt.subplot(n_test, 4, 4 * i + 1)
+        plt.title('Ground Truth')
+        plt.axis('off')
+        gt = imread(path + item[1])
+        plt.imshow(np.clip(gt, 0, 1))
+
+        plt.subplot(n_test, 4, 4 * i + 2)
+        plt.title('Prediction')
+        plt.axis('off')
+        labeled = np.argmax(output[i], axis=-1)
+        plt.imshow(labeled)
+
+        plt.subplot(n_test, 4, 4 * i + 3)
+        plt.title('Heat map')
+        plt.axis('off')
+        plt.imshow(output[i][:, :, 1])
+
+        plt.subplot(n_test, 4, 4 * i + 4)
+        plt.title('Comparison')
+        plt.axis('off')
+        rgb = np.empty((img_h, img_w, 3))
+        rgb[:, :, 0] = labeled
+        rgb[:, :, 1] = imread(path + item[0])
+        rgb[:, :, 2] = gt
+        plt.imshow(rgb)
+
+    plt.savefig('result.png')
+    plt.show()
+"""
+
+#########################################################################################################
+"""
+def SegNet(input_shape=(960, 736, 3), classes=13):
+    # c.f. https://github.com/alexgkendall/SegNet-Tutorial/blob/master/Example_Models/bayesian_segnet_camvid.prototxt
+    img_input = Input(shape=input_shape)
+    x = img_input
+    # Encoder
+    x = Convolution2D(64, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(128, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(256, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(512, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    # Decoder
+    x = Convolution2D(512, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(256, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(128, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(64, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = Convolution2D(classes, 1, 1, border_mode="valid")(x)
+    x = Reshape((input_shape[0]*input_shape[1], classes))(x)
+    x = Activation("softmax")(x)
+    model = Model(img_input, x)
+    return model
+"""
+
+from keras.layers import Input
+from keras.layers.convolutional import Convolution2D
+from keras.layers.core import Activation, Reshape
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+#from layers import MaxPoolingWithArgmax2D, MaxUnpooling2D
+
+
+import numpy as np
+#import matplotlib.pyplot as plt
+#from pylab import *
+import os
+import sys
+#from keras_contrib.applications import densenet
+from keras.models import Model
+from keras.regularizers import l2
+from keras.layers import *
+#from keras.engine import Layer
+from keras.applications.vgg16 import *
+from keras.models import *
+#from keras.applications.imagenet_utils import _obtain_input_shape
+import keras.backend as K
+import tensorflow as tf
+
+from utils.get_weights_path import *
+from utils.basics import *
+from utils.resnet_helpers import *
+from utils.BilinearUpSampling import *
+
+def FCN_Vgg16_32s(input_shape=(512, 512, 3), weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=2):
+    if batch_shape:
+        img_input = Input(batch_shape=batch_shape)
+        image_size = batch_shape[1:3]
+    else:
+        img_input = Input(shape=input_shape)
+        image_size = input_shape[0:2]
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', kernel_regularizer=l2(weight_decay))(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', kernel_regularizer=l2(weight_decay))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Convolutional layers transfered from fully-connected layers
+    x = Conv2D(4096, (7, 7), activation='relu', padding='same', name='fc1', kernel_regularizer=l2(weight_decay))(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(4096, (1, 1), activation='relu', padding='same', name='fc2', kernel_regularizer=l2(weight_decay))(x)
+    x = Dropout(0.5)(x)
+    #classifying layer
+    x = Conv2D(classes, (1, 1), kernel_initializer='he_normal', activation='linear', padding='valid', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+
+    x = BilinearUpSampling2D(size=(32, 32))(x)
+    x = Reshape((512*512, 2))(x)
+    x = Activation("softmax")(x)
+
+    model = Model(img_input, x)
+
+    #weights_path = os.path.expanduser(os.path.join('~', '.keras/models/fcn_vgg16_weights_tf_dim_ordering_tf_kernels.h5'))
+    #model.load_weights(weights_path, by_name=True)
+    return model
+
+#########################################################################################################
+def SegNet_compress(layer1_filters, layer2_filters, layer3_filters, layer4_filters, layer5_filters, layer6_filters, layer7_filters, layer8_filters,layer9_filters, layer10_filters, layer11_filters, layer12_filters, layer13_filters, layer14_filters, layer15_filters, layer16_filters,layer17_filters, layer18_filters, layer19_filters, layer20_filters, layer21_filters, layer22_filters, layer23_filters, layer24_filters, layer25_filters,input_shape, n_labels, kernel=3, pool_size=(2, 2), output_mode="softmax"):
+    # c.f. https://github.com/alexgkendall/SegNet-Tutorial/blob/master/Example_Models/bayesian_segnet_camvid.prototxt
+    inputs = Input(shape=input_shape)
+
+    conv_1 = Convolution2D(layer1_filters, (kernel, kernel), padding="same")(inputs)
+    conv_1 = BatchNormalization()(conv_1)
+    conv_1 = Activation("relu")(conv_1)
+    conv_2 = Convolution2D(layer2_filters, (kernel, kernel), padding="same")(conv_1)
+    conv_2 = BatchNormalization()(conv_2)
+    conv_2 = Activation("relu")(conv_2)
+
+    pool_1, mask_1 = MaxPoolingWithArgmax2D(pool_size)(conv_2)
+
+    conv_3 = Convolution2D(layer3_filters, (kernel, kernel), padding="same")(pool_1)
+    conv_3 = BatchNormalization()(conv_3)
+    conv_3 = Activation("relu")(conv_3)
+    conv_4 = Convolution2D(layer4_filters, (kernel, kernel), padding="same")(conv_3)
+    conv_4 = BatchNormalization()(conv_4)
+    conv_4 = Activation("relu")(conv_4)
+
+    pool_2, mask_2 = MaxPoolingWithArgmax2D(pool_size)(conv_4)
+
+    conv_5 = Convolution2D(layer5_filters, (kernel, kernel), padding="same")(pool_2)
+    conv_5 = BatchNormalization()(conv_5)
+    conv_5 = Activation("relu")(conv_5)
+    conv_6 = Convolution2D(layer6_filters, (kernel, kernel), padding="same")(conv_5)
+    conv_6 = BatchNormalization()(conv_6)
+    conv_6 = Activation("relu")(conv_6)
+    conv_7 = Convolution2D(layer7_filters, (kernel, kernel), padding="same")(conv_6)
+    conv_7 = BatchNormalization()(conv_7)
+    conv_7 = Activation("relu")(conv_7)
+
+    pool_3, mask_3 = MaxPoolingWithArgmax2D(pool_size)(conv_7)
+
+    conv_8 = Convolution2D(layer8_filters, (kernel, kernel), padding="same")(pool_3)
+    conv_8 = BatchNormalization()(conv_8)
+    conv_8 = Activation("relu")(conv_8)
+    conv_9 = Convolution2D(layer9_filters, (kernel, kernel), padding="same")(conv_8)
+    conv_9 = BatchNormalization()(conv_9)
+    conv_9 = Activation("relu")(conv_9)
+    conv_10 = Convolution2D(layer10_filters, (kernel, kernel), padding="same")(conv_9)
+    conv_10 = BatchNormalization()(conv_10)
+    conv_10 = Activation("relu")(conv_10)
+
+    pool_4, mask_4 = MaxPoolingWithArgmax2D(pool_size)(conv_10)
+
+    conv_11 = Convolution2D(layer11_filters, (kernel, kernel), padding="same")(pool_4)
+    conv_11 = BatchNormalization()(conv_11)
+    conv_11 = Activation("relu")(conv_11)
+    conv_12 = Convolution2D(layer12_filters, (kernel, kernel), padding="same")(conv_11)
+    conv_12 = BatchNormalization()(conv_12)
+    conv_12 = Activation("relu")(conv_12)
+    conv_13 = Convolution2D(layer13_filters, (kernel, kernel), padding="same")(conv_12)
+    conv_13 = BatchNormalization()(conv_13)
+    conv_13 = Activation("relu")(conv_13)
+
+    pool_5, mask_5 = MaxPoolingWithArgmax2D(pool_size)(conv_13)
+    print("Build enceder done..")
+
+    # decoder
+
+    unpool_1 = MaxUnpooling2D(pool_size)([pool_5, mask_5])
+
+    conv_14 = Convolution2D(layer14_filters, (kernel, kernel), padding="same")(unpool_1)
+    conv_14 = BatchNormalization()(conv_14)
+    conv_14 = Activation("relu")(conv_14)
+    conv_15 = Convolution2D(layer15_filters, (kernel, kernel), padding="same")(conv_14)
+    conv_15 = BatchNormalization()(conv_15)
+    conv_15 = Activation("relu")(conv_15)
+    conv_16 = Convolution2D(layer16_filters, (kernel, kernel), padding="same")(conv_15)
+    conv_16 = BatchNormalization()(conv_16)
+    conv_16 = Activation("relu")(conv_16)
+
+    unpool_2 = MaxUnpooling2D(pool_size)([conv_16, mask_4])
+
+    conv_17 = Convolution2D(layer17_filters, (kernel, kernel), padding="same")(unpool_2)
+    conv_17 = BatchNormalization()(conv_17)
+    conv_17 = Activation("relu")(conv_17)
+    conv_18 = Convolution2D(layer18_filters, (kernel, kernel), padding="same")(conv_17)
+    conv_18 = BatchNormalization()(conv_18)
+    conv_18 = Activation("relu")(conv_18)
+    conv_19 = Convolution2D(layer19_filters, (kernel, kernel), padding="same")(conv_18)
+    conv_19 = BatchNormalization()(conv_19)
+    conv_19 = Activation("relu")(conv_19)
+
+    unpool_3 = MaxUnpooling2D(pool_size)([conv_19, mask_3])
+
+    conv_20 = Convolution2D(layer20_filters, (kernel, kernel), padding="same")(unpool_3)
+    conv_20 = BatchNormalization()(conv_20)
+    conv_20 = Activation("relu")(conv_20)
+    conv_21 = Convolution2D(layer21_filters, (kernel, kernel), padding="same")(conv_20)
+    conv_21 = BatchNormalization()(conv_21)
+    conv_21 = Activation("relu")(conv_21)
+    conv_22 = Convolution2D(layer22_filters, (kernel, kernel), padding="same")(conv_21)
+    conv_22 = BatchNormalization()(conv_22)
+    conv_22 = Activation("relu")(conv_22)
+
+    unpool_4 = MaxUnpooling2D(pool_size)([conv_22, mask_2])
+
+    conv_23 = Convolution2D(layer23_filters, (kernel, kernel), padding="same")(unpool_4)
+    conv_23 = BatchNormalization()(conv_23)
+    conv_23 = Activation("relu")(conv_23)
+    conv_24 = Convolution2D(layer24_filters, (kernel, kernel), padding="same")(conv_23)
+    conv_24 = BatchNormalization()(conv_24)
+    conv_24 = Activation("relu")(conv_24)
+
+    unpool_5 = MaxUnpooling2D(pool_size)([conv_24, mask_1])
+
+    conv_25 = Convolution2D(layer25_filters, (kernel, kernel), padding="same")(unpool_5)
+    conv_25 = BatchNormalization()(conv_25)
+    conv_25 = Activation("relu")(conv_25)
+
+    conv_26 = Convolution2D(n_labels, (1, 1), padding="valid")(conv_25)
+    conv_26 = BatchNormalization()(conv_26)
+    conv_26 = Reshape(
+        (input_shape[0] * (input_shape[1]), n_labels),
+        input_shape=(input_shape[0], input_shape[1], n_labels),
+    )(conv_26)
+
+    outputs = Activation(output_mode)(conv_26)
+    print("Build decoder done..")
+
+    model = Model(inputs=inputs, outputs=outputs, name="SegNet")
+    return model
+
+"""
+with open('model_5l.json') as model_file:
+    autoencoder = models.model_from_json(model_file.read())
+"""
+
+autoencoder = FCN_Vgg16_32s()
+
+print('Start')
+optimizer = SGD(lr=0.001, momentum=0.9, decay=0.0005, nesterov=False)
+autoencoder.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+print( 'Compiled: OK')
+autoencoder.summary()
+
+# Train model or load weights
+
+train_data, train_label = prep_data1('train')
+val_data, val_label = prep_data1('val')
+nb_epoch = 100
+batch_size = 2
+history = autoencoder.fit(train_data, train_label, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(val_data, val_label))
+autoencoder.save_weights('model_5l_weight_fcn_lungs.hdf5')
+
+#autoencoder.load_weights('model_5l_weight_ep50.hdf5')
+
+
+layer1_filters = 64
+layer2_filters = 64
+layer3_filters = 128
+layer4_filters = 128
+layer5_filters = 256
+layer6_filters = 256
+layer7_filters = 256
+layer8_filters = 512
+layer9_filters = 512
+layer10_filters = 512
+layer11_filters = 512
+layer12_filters = 512
+layer13_filters = 512
+layer14_filters = 512
+layer15_filters = 512
+layer16_filters = 512
+layer17_filters = 512
+layer18_filters = 512
+layer19_filters = 256
+layer20_filters = 256
+layer21_filters = 256
+layer22_filters = 128
+layer23_filters = 128
+layer24_filters = 64
+layer25_filters = 64
+
+test_data, test_label = prep_data1('test')
+
+score = autoencoder.evaluate(test_data, test_label, verbose=1)
+print( 'Test score:', score[0])
+print( 'Test accuracy:', score[1])
+
